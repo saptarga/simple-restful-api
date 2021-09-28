f545b4557a6c2daaf5d4a40aece9a7679fb21048
saptarga <saptarga@gmail.com>
2021-09-28 17:58:35 +0700

    Add Log

--- src/main/java/com/saptarga/example/demo/dto/request/RequestTutorialDTO.java
+++ src/main/java/com/saptarga/example/demo/dto/request/RequestTutorialDTO.java
@@ -1,12 +1,10 @@
 package com.saptarga.example.demo.dto.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
+@ToString
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/com/saptarga/example/demo/service/Impl/TutorialServiceImpl.java
+++ src/main/java/com/saptarga/example/demo/service/Impl/TutorialServiceImpl.java
@@ -6,12 +6,14 @@ import com.saptarga.example.demo.model.Tutorial;
 import com.saptarga.example.demo.repository.TutorialRepository;
 import com.saptarga.example.demo.service.ITutorialService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class TutorialServiceImpl implements ITutorialService {
@@ -25,6 +27,7 @@ public class TutorialServiceImpl implements ITutorialService {
 
     @Override
     public Tutorial createTutorial(RequestTutorialDTO request) throws ServiceException {
+        log.info("Create New Tutorial With Request: {}", request.toString());
         return tutorialRepository.save(Tutorial.builder()
                 .title(request.getTitle())
                 .description(request.getDescription())
@@ -34,13 +37,17 @@ public class TutorialServiceImpl implements ITutorialService {
 
     @Override
     public Tutorial findById(Long id) throws Exception {
+        log.info("Find Tutorial With Id: {}", id.toString());
         return tutorialRepository.findById(id).orElse(null);
     }
 
     @Override
     public boolean deleteById(Long id) throws Exception {
+        log.warn("Delete Tutorial With Id: {}", id.toString());
+
         Tutorial tutorial = tutorialRepository.findById(id).orElse(null);
         if (Objects.isNull(tutorial)) {
+            log.error("Data Not Found With Id: {}", id.toString());
             throw new ServiceException("Data tutorial not found");
         }
 
@@ -50,8 +57,11 @@ public class TutorialServiceImpl implements ITutorialService {
 
     @Override
     public Tutorial updateById(Long id, RequestTutorialDTO request) throws Exception {
+        log.info("Update Tutorial With Id {} And Request {}", id.toString(), request.toString());
+
         Tutorial tutorial = tutorialRepository.findById(id).orElse(null);
         if (Objects.isNull(tutorial)) {
+            log.error("Data Not Found With Id: {}", id.toString());
             throw new ServiceException("Data tutorial not found");
         }
         tutorial.setTitle(request.getTitle());
